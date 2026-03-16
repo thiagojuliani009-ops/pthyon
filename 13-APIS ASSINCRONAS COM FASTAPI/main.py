ca12f8be2e82d8223cb2377836c6df2f1160f4d2
THIAGO JULIANI DE OLIVEIRA <thiagojuliani009@gmail.com>
2026-03-16 19:16:27 -0300

    Update main.py

--- 13-APIS ASSINCRONAS COM FASTAPI/main.py
+++ 13-APIS ASSINCRONAS COM FASTAPI/main.py
@@ -4,15 +4,15 @@ from fastapi import FastAPI
 app = FastAPI()
 
 fake_db = [
-    {"title": f"Criando uma aplicação com Django", "data": datetime.now(UTC)},
-    {"title": f"Criando uma aplicação com FastAPI", "data": datetime.now(UTC)},
-    {"title": f"Criando uma aplicação com Flask", "data": datetime.now(UTC)},
-    {"title": f"Criando uma aplicação com starlett", "data": datetime.now(UTC)},
+    {"title": f"Criando uma aplicação com Django", "data": datetime.now(UTC)"published": True},
+    {"title": f"Criando uma aplicação com FastAPI", "data": datetime.now(UTC)"published": True},
+    {"title": f"Criando uma aplicação com Flask", "data": datetime.now(UTC)"published": True},
+    {"title": f"Criando uma aplicação com starlett", "data": datetime.now(UTC)"published": True},
 ]
 
 @app.get("/posts")
-def read_posts(skip: int = 0, limit = len (fake_db)):   
-    return fake_db[skip: skip + limit]
+def read_posts(skip: int = 0, limit = len (fake_db), published: bool = True):   
+    return [ post for post in fake_db[skip: skip + limit] if post["published"] is published]
 
 @app.get("/posts/{framework}")
 def read_framework_posts(framework: str): 
