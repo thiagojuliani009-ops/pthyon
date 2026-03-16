7c4ac7e984ede784187f7407b48b2bc4eed8fa60
THIAGO JULIANI DE OLIVEIRA <thiagojuliani009@gmail.com>
2026-03-16 18:26:20 -0300

    novo

--- 13-APIS ASSINCRONAS COM FASTAPI/main.py
+++ 13-APIS ASSINCRONAS COM FASTAPI/main.py
@@ -4,16 +4,16 @@ from fastapi import FastAPI
 app = FastAPI()
 
 fake_db = [
-    {"title": f"Criando uma aplicação com Django", "data": datetime.now(UTC)}
-    {"title": f"Criando uma aplicação com FastApi", "data": datetime.now(UTC)}
+    {"title": f"Criando uma aplicação com Django", "data": datetime.now(UTC)},
+    {"title": f"Criando uma aplicação com FastAPI", "data": datetime.now(UTC)}
 ]
 
 @app.get("/posts")
-def read_posts: 
-   return fake_db
+def read_posts():  
+    return fake_db
 
 @app.get("/posts/{framework}")
-def read_fremeword_posts(framework: str): 
+def read_framework_posts(framework: str): 
     return {
         "posts": [
             {"title": f"Criando uma aplicação com {framework}", "data": datetime.now(UTC)},
