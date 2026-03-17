65903f2dfefc415c0a9d2d842149687b59abd8a5
THIAGO JULIANI DE OLIVEIRA <thiagojuliani009@gmail.com>
2026-03-17 11:26:29 -0300

    main

--- 16-Playwrigth/main.py
+++ 16-Playwrigth/main.py
@@ -4,6 +4,7 @@ with sync_playwright() as p:
     browser = p.chromium.launch(headless=False)
     page = browser.new_page()
     page.goto("https://")
+    page.goto("https://thiagojuliani009-ops.github.io/html-css/")
     print(page.title())
     browser.close()
 
