11a3928bd7232a34f78dce22ac58d7d0bb613376
THIAGO JULIANI DE OLIVEIRA <thiagojuliani009@gmail.com>
2026-03-17 09:36:15 -0300

    Playwright_teste

---
+++ 16-Playwrigth/main.py
@@ -0,0 +1,11 @@
+from playwright.sync_api import sync_playwright
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False)
+    page = browser.new_page()
+    page.goto("https://")
+    print(page.title())
+    browser.close()
+
+    print("olá, Mundo")
+
