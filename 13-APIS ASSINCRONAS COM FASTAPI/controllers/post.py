a08a0f414e966485e7a332e548c29427437d5843
THIAGO JULIANI DE OLIVEIRA <thiagojuliani009@gmail.com>
2026-03-23 14:58:11 -0300

    Ajustando lógica de filtro e nomes de variáveis

---
+++ 13-APIS ASSINCRONAS COM FASTAPI/controllers/post.py
@@ -0,0 +1,51 @@
+from datetime import UTC, datetime
+from typing import Annotated
+
+from fastapi import Response, Cookie, Response, status, APIRouter
+from schemas.post import PostIn
+from views.post import PostOut
+
+
+router = APIRouter()
+
+
+# Banco de dados fake
+fake_db = [
+    {"title": "Criando uma aplicação com Django", "data": datetime.now(UTC), "published": True},
+    {"title": "Criando uma aplicação com FastAPI", "data": datetime.now(UTC), "published": True},
+    {"title": "Criando uma aplicação com Flask", "data": datetime.now(UTC), "published": True},
+    {"title": "Criando uma aplicação com starlett", "data": datetime.now(UTC), "published": False},
+]
+
+
+@router.post("/posts/", status_code=status.HTTP_201_CREATED, response_model=PostOut)
+def create_post(post: PostIn):
+    fake_db.append(post.model_dump())
+    return post
+
+# Rota para ler posts com Filtro, Cookie e Header
+@router.get("/posts/" response_model=list[PostOut])
+def read_posts(
+    response: Response,
+    published: bool, 
+    limit: int,
+    skip: int = 0,
+    ads_id: Annotated[str | None, Cookie()] = None,
+    user_agent: Annotated[str | None, Header()] = None,
+):
+
+response.set_cookie(key="user", value="thiagojuliani009@gmail.com")
+print(f"Cookie: {ads_id}")
+print(f"User-agent: {user_agent}")
+tail = skip + limit
+return [post for post in fake_db[skip:tail] if post["published"] is published] 
+
+# Rota dinâmica por framework
+@router.get("/posts/{framework}", respone_model=PostOut)
+def read_framework_posts(framework: str): 
+    return {
+        "posts": [
+            {"title": f"Criando uma aplicação com {framework}", "data": datetime.now(UTC)},
+            {"title": f"Internacionalizando uma app {framework}", "data": datetime.now(UTC)}
+        ]
+    }
