22ad04a70e3cd3122a9a70ff1cd695f070effdb1
THIAGO JULIANI DE OLIVEIRA <thiagojuliani009@gmail.com>
2026-03-26 19:40:16 -0300

    delete

--- 13-APIS ASSINCRONAS COM FASTAPI/schemas/post.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from typing import Optional
-from datetime import datetime
-
-class PostIn(BaseModel):
-    title: str
-    content: str
-    published: bool = False
-    published_at: Optional[datetime] = None
