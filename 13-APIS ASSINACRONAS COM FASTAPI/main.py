16ba673f72acb2e1b1f38fa9d2f15f4ea1d4dffe
THIAGO JULIANI DE OLIVEIRA <thiagojuliani009@gmail.com>
2026-03-27 20:43:15 -0300

    Summary (required)

--- 13-APIS ASSINACRONAS COM FASTAPI/main.py
+++ 13-APIS ASSINACRONAS COM FASTAPI/main.py
@@ -1,8 +1,29 @@
+from contextlib import asynccontextmanager
+
+import databases
+import sqlalchemy as sa
 from fastapi import FastAPI
 from controllers import post
 
-app = FastAPI()
-app.include_router(post.router)
+
+
+DATABASE_URL= "sqlite: ///./blog.db"
+
+database = databases.Database(DATABASE_URL)
+metadata = sa.metadata()
+engine = sa.create_engine(DATABASE_URL, connect_args={"check_same_thread":False})
+metadata.create_all(engine)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await database.connect()
+    yield
+    await database.disconnect()
+
+app = FastAPI(lifespan=lifespan)
+app.include_routerr(post.router)
+
+
 
 
 
